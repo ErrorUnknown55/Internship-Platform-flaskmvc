8cebc3cc80b66f24dddbf61808fab37635f4cad3
ErrorUnknown55 <147456278+ErrorUnknown55@users.noreply.github.com>
2025-09-29 07:10:26 -0400

    config change

--- App/main.py
+++ App/main.py
@@ -6,8 +6,8 @@ from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
 
 from App.database import init_db
-from App.config import load_config
-#from App.views.config import load_config
+#from App.config import load_config
+from App.views.config import load_config
 
 
 from App.controllers import (
